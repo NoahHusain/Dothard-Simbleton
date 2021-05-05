c3cea80cfbf688c84b3fd79052ddcca30cd1ade9
Noah Husain <noahhusain@gmail.com>
2021-05-05 15:58:39 -0500

    Pushing current status of code to cloud. Working on getting search by first name and last name

---
+++ scripts/findAgent.js
@@ -0,0 +1,35 @@
+import { Agents } from "./AgentsHTML.js"
+import { getBusinesses } from "./database.js"
+
+
+const agentSearchResultArticle = document.querySelector(".foundAgents")
+
+
+document.querySelector("#agentFirstNameSearch").addEventListener("keypress", keyPressEvent => {
+    if(keyPressEvent.charCode === 13) {
+        const businesses = getBusinesses()
+
+        const input = keyPressEvent.target.value.toLowerCase()
+
+        const findAgentsBusiness = businesses.find(business => 
+            business.purchasingAgent.nameFirst.toLowerCase().includes(input) ||
+            business.purchasingAgent.nameLast.toLowerCase().includes(input))
+
+        const foundAgent = FindAgent(findAgentsBusiness)
+
+        const agentInfo = Agents(foundAgent)
+
+        agentSearchResultArticle.innerHTML = agentInfo
+    }
+})
+
+
+
+
+export const FindAgent = (agentsBusiness) => {
+    return {
+        fullName: `${agentsBusiness.purchasingAgent.nameFirst} ${agentsBusiness.purchasingAgent.nameLast}`,
+        company: agentsBusiness.companyName,
+        phoneNumber: agentsBusiness.phoneWork
+    }
+}
